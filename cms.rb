0103c6193964746d5ffeca84d807e08708d6e7f7
Ethan Weiner <ethanweiner20@gmail.com>
2022-04-02 15:29:49 -0400

    Handle attempts to access nonexistent files

--- cms.rb
+++ cms.rb
@@ -5,6 +5,11 @@ require 'tilt/erubis'
 # Guarantees that we access the root directory of `cms.rb`
 ROOT = File.expand_path(__dir__)
 
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
 def load_files
   Dir.glob("#{ROOT}/data/*").map { |path| File.basename(path) }
 end
@@ -16,6 +21,12 @@ end
 
 get '/:file' do
   file_path = "#{ROOT}/data/#{params[:file]}"
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+
+  if File.exist?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:file]} does not exist."
+    redirect "/"
+  end
 end
